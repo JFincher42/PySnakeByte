10413abd0033003f844db865479cac9bb2cdbb47
Jon Fincher <jfincher42@gmail.com>
2019-06-12 11:37:56 -0500

    Changed update to on_update

--- arcade_test.py
+++ arcade_test.py
@@ -2,10 +2,15 @@ import arcade
 
 
 class ArcadeTest(arcade.Window):
-
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK_LEATHER_JACKET)
+
+        # Set a center for the circle
+        self.center_x, self.center_y = 300, 250
+
+        # Set a speed
+        self.speed_x, self.speed_y = 13, 7
 
     def on_draw(self):
         arcade.start_render()
@@ -14,9 +19,7 @@ class ArcadeTest(arcade.Window):
             self.center_x, self.center_y, 10, arcade.color.WHEAT
         )
 
-
-
-    def update(self, delta):
+    def on_update(self, delta):
         self.center_x += self.speed_x
         self.center_y += self.speed_y
 
@@ -27,14 +30,6 @@ class ArcadeTest(arcade.Window):
 
 
 if __name__ == "__main__":
-
     arcade_test = ArcadeTest(800, 700, "Test")
 
-    # Set a center for the circle
-    arcade_test.center_x, arcade_test.center_y = 300, 250
-
-    # Set a speed
-    arcade_test.speed_x, arcade_test.speed_y = 10, 9
-
-    # arcade.schedule(on_draw, 1 / 30)
     arcade.run()
